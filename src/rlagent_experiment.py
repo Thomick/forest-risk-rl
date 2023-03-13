65fe2b95595780944fa16f21b9054ef37438c10b
Thomas MICHEL <thomas.michel.thmi@gmail.com>
2023-03-13 16:10:30 +0100

    update requirements and improve rl experiment

--- src/rlagent_experiment.py
+++ src/rlagent_experiment.py
@@ -26,13 +26,15 @@ from tqdm import tqdm
 
 row, col = 5, 5
 H = 20
-nb_run = 500
+nb_run = 10
 nb_iter = 100
+nb_epoch = 1000
 budget = 100
 alpha = 0.2
 beta = 0.1
 with_storms = True
 storm_probability = 0.05
+load_model = True
 
 nb_tree = row * col
 adjacency_matrix = make_grid_matrix(row, col)
@@ -49,42 +51,45 @@ else:
     env = ForestLinearEnv(nb_tree, adjacency_matrix, H=H, alpha=alpha, beta=beta)
 
 agent = PPO("MlpPolicy", env, verbose=1)
-agent.learn(total_timesteps=budget)
+if load_model:
+    agent = PPO.load("ppo_forest", env)
+    nb_epoch = 1
+else:
+    agent.learn(total_timesteps=budget)
 
-df = pd.DataFrame(columns=["Run", "Total reward"])
+df = pd.DataFrame(columns=["Epoch", "Total reward"])
 
 states = []
 actions = []
-for j in tqdm(range(nb_run), desc="Evaluating agent"):
-    agent.learn(total_timesteps=budget)
-    all_total_rewards = []
-    harvest_sizes = []
-    observation = env.reset()
-    if j == (nb_run - 1):
+for j in tqdm(range(nb_epoch), desc="Evaluating agent"):
+    if not load_model:
+        agent.learn(total_timesteps=budget)
+    r = []
+    states = []
+    actions = []
+    for k in range(nb_run):
+        observation = env.reset()
         states.append(observation)
-    total_reward = 0
-    harvest_count = 0
-    time_last_harvest = [0] * nb_tree
-    cutting_age = []
-    for i in range(nb_iter):
-        action, _ = agent.predict(observation)
-        observation, reward, done, _ = env.step(action)
-        total_reward += reward
-        if j == (nb_run - 1):
+        total_reward = 0
+        for i in range(nb_iter):
+            action, _ = agent.predict(observation)
+            observation, reward, done, _ = env.step(action)
+            total_reward += reward
             states.append(observation)
             actions.append(action)
-    df.loc[len(df)] = [
-        j,
-        total_reward,
-    ]
-print(df)
-sns.lineplot(data=df, x="Run", y="Total reward")
+        df.loc[len(df)] = [j, total_reward]
+        r.append(total_reward)
+    print(np.mean(r))
+if not load_model:
+    agent.save("ppo_forest")
+# print(df)
+sns.lineplot(data=df, x="Epoch", y="Total reward")
 plt.figure()
-plt.plot(states)
+plt.plot(states[0:nb_iter])
 action_array = np.zeros(nb_tree)
 for a in actions:
     action_array += a
 plt.figure()
-plt.imshow(action_array.reshape(row, col))
+plt.imshow(action_array.reshape(row, col) / nb_run)
 plt.colorbar()
 plt.show()
